7c9b0a7bf737d7c7e642420a989a6296f0c75289
Richard Shea <rshea@thecubagroup.com>
2025-08-23 22:23:25 +1200

    Add geography.views functionality to allow for creating Country/City.

--- djangarnish/geography/views.py
+++ djangarnish/geography/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CountryForm, CityForm
 
-# Create your views here.
+# ------------------------------
+# View for creating a Country
+# ------------------------------
+def country_create(request):
+    if request.method == 'POST':
+        form = CountryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # adjust this to your home page or country list
+    else:
+        form = CountryForm()
+
+    return render(request, 'geography/country_form.html', {'form': form})
+
+
+# ------------------------------
+# View for creating a City
+# ------------------------------
+def city_create(request):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # adjust this to your home page or city list
+    else:
+        form = CityForm()
+
+    return render(request, 'geography/city_form.html', {'form': form})
+
